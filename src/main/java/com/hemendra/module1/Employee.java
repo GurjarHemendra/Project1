daac8afe1b7d093d77094c8241779981699f09a3
GurjarHemendra <connecthemendra29@gmail.com>
2022-09-13 07:47:45 +0530

    Initial commit

---
+++ src/main/java/com/hemendra/module1/Employee.java
@@ -0,0 +1,40 @@
+package com.hemendra.module1;
+
+public class Employee {
+
+    static
+    {
+        System.out.println("this is static block");
+    }
+    private String name;
+    private String sal;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSal() {
+        return sal;
+    }
+
+    public void setSal(String sal) {
+        this.sal = sal;
+    }
+
+    public Employee(String name, String sal) {
+        this.name = name;
+        this.sal = sal;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", sal='" + sal + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/hemendra/module1/first.java
@@ -0,0 +1,9 @@
+package com.hemendra.module1;
+
+public class first {
+    public static void main(String[] args) {
+        Employee hemendra = new Employee("hemendra", "30000");
+//        ctrl+alt+v  : assign variable to local feilld
+        System.out.println(hemendra);
+    }
+}
